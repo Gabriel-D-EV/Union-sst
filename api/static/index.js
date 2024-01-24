e5cb192a8df439602817c103f68ef5e8f5a8f1f2
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-24 16:31:38 -0300

    menu hamb open close

--- api/static/index.js
+++ api/static/index.js
@@ -3,3 +3,16 @@ window.addEventListener('scroll', function() {
     header.classList.toggle("newheader",window.scrollY > 0)
 })
 
+const ham = document.querySelector(".mobile");
+const nav = document.querySelector(".nav-list");
+
+ham.addEventListener("click", () => nav.classList.toggle("active"));
+
+const navlink = document.querySelector(".nav-list")
+
+
+navlink.addEventListener("click", () => {
+    if (nav.classList.contains("active")) {
+        nav.classList.remove("active");
+    } else { nav.classList.add("active"); }
+})
